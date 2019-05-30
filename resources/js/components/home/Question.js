a3f793e0e177b768cc0d33ffbbf23483a43c6553
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2019-05-30 09:35:30 -0400

    questionnaire done, functionality to add

--- resources/js/components/home/Question.js
+++ resources/js/components/home/Question.js
@@ -2,80 +2,81 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import Select from 'react-select';
 
 
 import Multiselect from 'multiselect-dropdown-react';
 
+
 const data = [{
-    name: 'Agriculture, foresterie, peche et chasse',
+    label: 'Agriculture, foresterie, peche et chasse',
     value: 'SCIAN 11'
 },
     {
-        name: 'Extraction miniere et extracion de petrole et de gaz',
+        label: 'Extraction miniere et extracion de petrole et de gaz',
         value: 'SCIAN 21'
     },
     {
-        name: 'Services publics',
+        label: 'Services publics',
         value: 'SCIAN 22'
     },
     {
-        name: 'Construction',
+        label: 'Construction',
         value: 'SCIAN 23'
     },
     {
-        name: 'Fabrication',
+        label: 'Fabrication',
         value: 'SCIAN 31-33'
     },
     {
-        name: 'Commerce de gros',
+        label: 'Commerce de gros',
         value: 'SCIAN 41'
     },
     {
-        name: 'Commerce de détail',
+        label: 'Commerce de détail',
         value: 'SCIAN 44-45'
     },
     {
-        name: 'Transport et entreposage',
+        label: 'Transport et entreposage',
         value: 'SCIAN 48-49'
     },
     {
-        name: 'Industrie de l\'information et industrie culturelle',
+        label: 'Industrie de l\'information et industrie culturelle',
         value: 'SCIAN 51'
     },
     {
-        name: 'Finance et assurances',
+        label: 'Finance et assurances',
         value: 'SCIAN 52'
     },
     {
-        name: 'Services d\'immobiliers et services de location et de location à bail',
+        label: 'Services d\'immobiliers et services de location et de location à bail',
         value: 'SCIAN 53'
     },
     {
-        name: 'Services professionnels, scientifiques et techniques',
+        label: 'Services professionnels, scientifiques et techniques',
         value: 'SCIAN 54'
     },
     {
-        name: 'Gestion de sociétés et d\'entreprises',
+        label: 'Gestion de sociétés et d\'entreprises',
         value: 'SCIAN 55'
     },
     {
-        name: 'Services administratifs, services de soutien, services de gestion de déchets et services d\'assainissement',
+        label: 'Services administratifs, services de soutien, services de gestion de déchets et services d\'assainissement',
         value: 'SCIAN 56'
     },
     {
-        name: 'Soins de santé et assistance sociale',
+        label: 'Soins de santé et assistance sociale',
         value: 'SCIAN 62'
     },
     {
-        name: 'Arts, spectacles et loisirs',
+        label: 'Arts, spectacles et loisirs',
         value: 'SCIAN 71'
     },
-    {   name: 'Hébergement et services de restauration',
+    {   label: 'Hébergement et services de restauration',
         value: 'SCIAN 72'
     },
     {
-        name: 'Autres services – sauf les administrations publiques',
+        label: 'Autres services – sauf les administrations publiques',
         value: 'SCIAN 81'
     },];
 
@@ -109,7 +110,8 @@ export default class Question extends React.Component {
             City: "",
             PostalCode: "",
             CorporateAddress: "",
-                selectedOption: ""
+                onSelectOptions: "",
+
 
         };
     }
@@ -138,6 +140,11 @@ export default class Question extends React.Component {
         });
     }
 
+    showHideDiv(corpAddress) {
+        var box = document.getElementById("corpAddress");
+        box.style.display = checkCorp.checked ? "block" : "none";
+
+    }
 
 
 
@@ -215,17 +222,20 @@ export default class Question extends React.Component {
                 {/*group 2*/}
                 <Form.Group controlId="BusinessInformation">
                     <Col sm="5">
-                    <Form.Control name = "BusinessName" type="business" placeholder="business name" onChange={this.handleChange} value={this.state.BusinessName}/>
+                        <Form.Label>Business Name</Form.Label>
+                    <Form.Control name = "BusinessName" type="business" placeholder="Business Name" onChange={this.handleChange} value={this.state.BusinessName}/>
                     <Form.Group controlId="BusinessAddress">
-
+                        <Form.Label>Quebec Address</Form.Label>
                     <Form.Control name="QuebecAddress" type="address" placeholder="Quebec address" onChange={this.handleChange} value={this.state.QuebecAddress}/>
+                    <Form.Label>City</Form.Label>
                     <Form.Control name="City" type="city" placeholder="city" onChange={this.handleChange} value={this.state.City} />
+                    <Form.Label>Postal Code</Form.Label>
                     <Form.Control name="PostalCode" type="PostalCode" placeholder="Postal Code" onChange={this.handleChange} value={this.state.PostalCode}/>
 
 
-                    <Form.Check type="checkbox" label="different corporate address"/>
-                    <Form.Control name="CorporateAddress" type="address2" placeholder="corporate address" onChange={this.handleChange} value={this.state.CorporateAddress}/>
-S
+                    <Form.Check controlId ="checkCorp" type="checkbox" label="different corporate address" />
+                    <Form.Control controlId = "corpAddress" name="CorporateAddress" type="address2" placeholder="corporate address" onChange={this.handleChange} value={this.state.CorporateAddress}/>
+
                     </Form.Group>
                     </Col>
                 </Form.Group>
@@ -249,7 +259,7 @@ S
                     <Form.Text className="text-muted">
                         Select 1 or multiple
                     </Form.Text>
-                    <Multiselect options={data} onSelectOptions={this.result}/>
+                    <Select options={data} />
                     </Col>
 
 
@@ -259,20 +269,43 @@ S
                 <Form.Group>
                     <Col sm="5">
                 <Form.Label>How is the business classed</Form.Label>
-                <Form.Check type= "radio" label={'Société par actions'} />
-                <Form.Check type="radio" label={'Entreprise individuelle'} />
-                <Form.Check type="radio" label={'Société en nom collectif'} />
+                <Form.Check type= "radio" label={'Société par actions'} name="formHorizontalRadios0"
+                            id="formHorizontalRadios1" />
+                <Form.Check type="radio" label={'Entreprise individuelle'} name="formHorizontalRadios0"
+                            id="formHorizontalRadios1"/>
+                <Form.Check type="radio" label={'Société en nom collectif'} name="formHorizontalRadios0"
+                            id="formHorizontalRadios1"/>
                 <Form.Check type="radio" label={'Coopérative'} />
-                <Form.Check type="radio" label={'Organisme sans but lucratif'} />
-                <Form.Check type="radio" label={'autre'} />
+                <Form.Check type="radio" label={'Organisme sans but lucratif'} name="formHorizontalRadios0"
+                            id="formHorizontalRadios1"/>
+                <Form.Check type="radio" label={'autre'} name="formHorizontalRadios"
+                            id="formHorizontalRadios1"/>
                     </Col>
 
                 </Form.Group>
 
                 <Form.Group>
 
-                <Form.Label>Number of employees at (var company name)</Form.Label>
 
+                    <Form.Group as={Col} controlId="formGridState">
+
+                        <Col sm="5">
+                            <Form.Label>Number of employees</Form.Label>
+                            <Form.Control as="select">
+                                <option>Select Range</option>
+                                <option> {"<"} 99,999$" </option>
+                                <option> 100,000$ - 449,999$ </option>
+                                <option> 500,000$ - 999,999$ </option>
+                                <option> 1,000,000$ - 4,999,999$ </option>
+                                <option> 5,000,000$ - 9,999,999$ </option>
+                                <option> 10,000,000$ - 24,999,999$ </option>
+                                <option> 25,000,000$ - 49,999,999$ </option>
+                                <option> 50,000,000$ - 99,999,999$ </option>
+                                <option> 100,000,000$ - 249,999,999$ </option>
+                                <option> {">"} 250,000,000$ </option>
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
 
                 </Form.Group>
 
@@ -287,6 +320,8 @@ S
                             label="Yes"
                             name="formHorizontalRadios"
                             id="formHorizontalRadios1"
+                            value="option1"
+                            checked={this.state.selectedOption === "option1"}
                         />
                         <Form.Check
                             type="radio"
@@ -294,8 +329,7 @@ S
                             name="formHorizontalRadios"
                             id="formHorizontalRadios2"
                         />
-                        <radio name="group">h</radio>
-                        <radio name="group">j</radio>
+
                     </Col>
                 </Form.Group>
 
@@ -306,13 +340,13 @@ S
                         <Form.Check
                             type="radio"
                             label="Yes"
-                            name="formHorizontalRadios"
+                            name="formHorizontalRadios1"
                             id="formHorizontalRadios1"
                         />
                         <Form.Check
                             type="radio"
                             label="No"
-                            name="formHorizontalRadios"
+                            name="formHorizontalRadios1"
                             id="formHorizontalRadios2"
                         />
                     </Col>
@@ -325,25 +359,25 @@ S
                         <Form.Check
                             type="radio"
                             label="Regional"
-                            name="formHorizontalRadios"
+                            name="formHorizontalRadios2"
                             id="formHorizontalRadios1"
                         />
                         <Form.Check
                             type="radio"
                             label="Provincial"
-                            name="formHorizontalRadios"
+                            name="formHorizontalRadios2"
                             id="formHorizontalRadios2"
                         />
                         <Form.Check
                             type="radio"
                             label="National"
-                            name="formHorizontalRadios"
+                            name="formHorizontalRadios2"
                             id="formHorizontalRadios3"
                         />
                         <Form.Check
                             type="radio"
                             label="International"
-                            name="formHorizontalRadios"
+                            name="formHorizontalRadios2"
                             id="formHorizontalRadios4"
                         />
                     </Col>
@@ -353,23 +387,14 @@ S
                     <Col sm="5">
                     <Form.Label>What is your general client base
                     </Form.Label>
-                    <Form.Check type="radio" label="Individuals" name="formHorizontalRadios" id="formHorizontalRadios1" />
-                    <Form.Check type="radio" label="Businesses" name="formHorizontalRadios" id="formHorizontalRadios2" />
-                    <Form.Check type="radio" label="Buying groups" name="formHorizontalRadios" id="formHorizontalRadios3" />
-                    <Form.Check type="radio" label="Resellers or Distributors" name="formHorizontalRadios" id="formHorizontalRadios4" />
+                    <Form.Check type="radio" label="Individuals" name="formHorizontalRadios3" id="formHorizontalRadios1" />
+                    <Form.Check type="radio" label="Businesses" name="formHorizontalRadios3" id="formHorizontalRadios2" />
+                    <Form.Check type="radio" label="Buying groups" name="formHorizontalRadios3" id="formHorizontalRadios3" />
+                    <Form.Check type="radio" label="Resellers or Distributors" name="formHorizontalRadios3" id="formHorizontalRadios4" />
                     </Col>
                 </Form.Group>
 
-                <Form.Group>
-                    <Col sm="5">
-                    <Form.Label>What is your general client base
-                    </Form.Label>
-                    <Form.Check type="radio" label={'Individuals'} name="formHorizontalRadios" id="formHorizontalRadios1" />
-                    <Form.Check type="radio" label={'Businesses'} name="formHorizontalRadios" id="formHorizontalRadios2"/>
-                    <Form.Check type="radio" label={'Buying groups'} name="formHorizontalRadios" id="formHorizontalRadios3"/>
-                    <Form.Check type="radio" label={'Resellers or Distributors'} name="formHorizontalRadios" id="formHorizontalRadios4"/>
-                    </Col>
-                </Form.Group>
+
 
                 <Form.Group as={Col} controlId="formGridState">
 
