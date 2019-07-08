a6e3489954fe54e7b7e98dfea2c8d0edafbefa32
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2019-07-08 10:26:52 -0400

    test error css
    commented out index.css and animate.css to see where the problem lies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,14 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
           integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link href={{ URL::asset('css/index.css') }} rel="stylesheet">
+    <!-- link href={{ secure_asset(URL::asset('css/index.css')) }} rel="stylesheet" -->
     <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossorigin="anonymous"
     />
-    <link rel="stylesheet" href="animate.min.css">
+    <!-- link rel="stylesheet" href="animate.min.css" -->
 </head>
 <body>
 
