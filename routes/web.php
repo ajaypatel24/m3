21f63f4242d5e26f0516ea479fb3485faccb4c11
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2019-07-08 16:26:20 -0400

    added comments on method functionality

--- routes/web.php
+++ routes/web.php
@@ -20,38 +20,38 @@
  */
 /*  */
 
-
+/* Login Controller */
 Route::get('/', 'LoginController@index');
 
-//Route::post('/login', 'LoginController@login');
+/* Register Controller */
+Route::post('/register', 'RegisterController@register'); //register new user
 
-Route::post('/register', 'RegisterController@register');
-
+/* Prestart Controller */
 Route::get('/prestart', 'PrestartController@index');
-
-Route::get('/user/{Id}', 'PrestartController@search');
-Route::get('/scian/{Id}', 'PrestartController@searchSCIAN');
-Route::get('/name/{Id}', 'PrestartController@getName' );
-
-Route::post('/','PrestartController@prestartSave'); /*method from php controller */
-Route::post('/intrants', 'InventaireController@store');
-Route::post('/categorie', 'CategorieController@tableEnergySave');
-Route::get('/info', 'CategorieController@p');
+Route::get('/user/{Id}', 'PrestartController@search'); //gets email address
+Route::get('/scian/{Id}', 'PrestartController@searchSCIAN'); //gets Scian from prestart questions
+Route::get('/name/{Id}', 'PrestartController@getName' ); //gets name of user
 Route::get('/data', 'PrestartController@p');
-
+Route::post('/','PrestartController@prestartSave'); /*method from php controller */
 Route::get('/querytest', 'PrestartController@testQuery');
 
+/* Inventaire Controller */
+Route::post('/intrants', 'InventaireController@store');
+Route::post('/intrants/{Id}', 'InventaireController@addIntrant'); //add new intrant
+Route::get('intrants/{Id}', 'InventaireController@getIntrant'); //gets intrants
+Route::get('/inventaire/{Id}', 'InventaireController@inventaireData'); //gets energy table data
+Route::post('delIntrants/{name}/{id}', 'InventaireController@deleteIntrant'); //delete single intrant
+
+/* Categorie Controller */
+Route::post('/categorie', 'CategorieController@tableEnergySave');
+Route::get('/info', 'CategorieController@p');
+
+/* Register Controller */
+Route::post('/contact/{Id}', 'RegisterController@contactInfo'); //add extra user info
+Route::get('/contact/{Id}', 'RegisterController@profileInfo'); //gets all user info
 
 
-Route::post('/contact/{Id}', 'RegisterController@contactInfo');
 
-Route::get('/contact/{Id}', 'RegisterController@profileInfo');
-Route::get('/inventaire/{Id}', 'InventaireController@inventaireData');
-
-Route::post('/intrants/{Id}', 'InventaireController@addIntrant');
-
-Route::get('intrants/{Id}', 'InventaireController@getIntrant');
-Route::post('delIntrants/{name}/{id}', 'InventaireController@deleteIntrant');
 
 
 
