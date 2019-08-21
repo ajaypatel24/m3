23e74e4318ffc8d6f7ea5b9df03b6622987d439c
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2019-08-21 13:13:23 -0400

    test

--- database/migrations/2019_07_11_165522_type_vehicule.php
+++ database/migrations/2019_07_11_165522_type_vehicule.php
@@ -16,19 +16,20 @@ class TypeVehicule extends Migration
         Schema::create('typevehicule', function (Blueprint $table) {
 
             $table->string('idVehicule')->primary();
+            $table->integer('Num_Affiche');
             $table->string('Lib_VehiculeEN',60)->nullable();
             $table->string('Lib_VehiculeFR',60)->nullable();
             $table->string('Type_transport',10)->nullable();
             $table->decimal('Coeff_GES_km', 9,8)->default(0);
             $table->integer('PoidsChargement')->default(0);
             $table->decimal('VolumeChargement',6,2)->default(0);
-            $table->string('Identifie_BD', 10)->nullable();
+            $table->string('Identifie_BD', 30)->nullable();
             $table->string('Nom_BD', 80)->nullable();
             $table->string('Categorie1', 200)->nullable();
             $table->decimal('Score_CC', 7,5)->default(0);
             $table->decimal('Score_SH', 14,12)->default(0);
             $table->decimal('Score_EcoS', 15,14)->default(0);
-            $table->decimal('Score_Res', 10,6)->default(0);
+            $table->decimal('Score_Res', 9,8)->default(0);
             $table->string('Informations_Add', 255)->nullable();
             $table->timestamps();
         });
